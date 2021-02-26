dd7fdc7626701a010f7ccb01d014c51a7a789215
168808 <79679543+168808@users.noreply.github.com>
2021-02-26 13:35:45 +0800

    Update nav.config.ts

--- nav.config.ts
+++ nav.config.ts
@@ -2,7 +2,7 @@ import { IConfig } from './src/types'
 
 const c: IConfig = {
   // [必填], 请填写您的仓库地址
-  gitRepoUrl: 'https://github.com/xjh22222228/nav',
+  gitRepoUrl: 'https://github.com/168808/nav',
 
   // 部署分支
   branch: 'main',
@@ -14,16 +14,16 @@ const c: IConfig = {
   showGithub: true,
 
   // 您的网站地址，有利于SEO
-  homeUrl: 'https://nav3.cn',
+  homeUrl: 'https://aldh.cc',
 
   // 网站标题
-  title: '发现导航 - 精选实用导航网站',
+  title: '阿拉导航 - 精选实用导航网站',
 
   // 网站描述
-  description: '发现导航 - 精选实用导航网站',
+  description: '阿拉导航 - 精选实用导航网站',
 
   // 网站关键字
-  keywords: '导航,前端资源,社区站点,设计师,实用工具,学习资源,运营,网络安全,node.js',
+  keywords: '导航,资源,社区站点,实用工具,学习资源,运营',
 
   // 默认主题: Light | Sim | Side | App
   theme: 'Light',
@@ -31,20 +31,20 @@ const c: IConfig = {
   // 网站底部内容, 版权信息、备案号, 可以是 HTML
   footerContent: `
     <div style="font-weight: bold;">共收录\${total}个网站</div>
-    <div>Copyright © 2018-2021 nav3.cn, All Rights Reserved</div>
+    <div>Copyright © 2021-2061 aldh.cc, All Rights Reserved</div>
   `,
 
   // 百度统计地址
   // https://tongji.baidu.com/web/welcome/login
-  baiduStatisticsUrl: 'https://hm.baidu.com/hm.js?4582be7af7e7c95ef75351e07c6c32ba',
+  baiduStatisticsUrl: 'https://hm.baidu.com/hm.js?e1be6a654aa48c5e48ad64a6f94f6e78',
 
   // CNZZ 统计地址
-  // https://www.cnzz.com/o_index.php
+  // 
   cnzzStatisticsUrl: '',
 
   // Light 主题配置
   lightThemeConfig: {
-    // https://www.nav3.cn/#/light?q=grabient
+    // https://aldh.cc/#/light?q=grabient
     backgroundLinear: [
       'linear-gradient(62deg, #8EC5FC 0%, #E0C3FC 100%)',
       'linear-gradient(90deg, #FEE140 0%, #FA709A 100%)',
@@ -79,7 +79,7 @@ const c: IConfig = {
   simThemeConfig: {
     // 海报图
     posterImageUrls: [
-      'https://raw.sevencdn.com/xjh22222228/nav/image/sim-wallpaper.jpg'
+      'https://raw.sevencdn.com/imagehosted/images/master/2019/06/20190612115457.png'
     ],
     description: '这里收录多达 <b>${total}</b> 个优质网站， 助您工作、学习和生活'
   }
